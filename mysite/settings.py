9b9344a8b2ef6beb2eaa179e37dcc9f58a389047
김승진 <kimsj1993@gmail.com>
2019-02-07 23:11:09 -0600

    add login module

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,6 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+LOGIN_REDIRECT_URL = '/'
+
+
 try:
     from .local_settings import *
 except ImportError:
